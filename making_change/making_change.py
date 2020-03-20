eef65d69fec7d3a6e67c312e6fa3ebfbc39a71fe
xander-bennett <xander-bennett@users.noreply.github.com>
2020-03-19 18:05:51 -0700

    uploading making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,22 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
-
+  # base case
+  if amount <=0:
+    return 1
+  #need to create a reference for all possible ways to make change
+  #done at length of amount+1 since range indexes at 0 to start
+  possibilities = [0 for x in range(amount+1)]
+  # base case for how many ways there are for making change for 0
+  possibilities[0] = 1
+  # with base cases established, we will loop through each denomination
+  for i in range(0, len(denominations)):
+    # for every denomination as it loops, comparison will be made between that denomination and the initial amount
+      for j in range(denominations[i], amount+1):
+        #with every value in possibilities, there is an increment
+        # each increment is added after the value of whatever denomination is taken away
+        possibilities[j] += possibilities[j-denominations[i]]
+  return possibilities[amount]
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
